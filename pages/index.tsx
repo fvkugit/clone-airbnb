2cd3fd3b6bda70a52b8788483800e5c0ea4a71a0
Facundo <facugastonbarral@gmail.com>
2023-01-20 11:41:14 -0300

    Add Header component to Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header.js'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-    <h1>Building Airbnb Clone</h1>
+    <Header/>
     </div>
   )
 }
