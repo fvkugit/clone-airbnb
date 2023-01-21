2afee2c083006a4252f92870b99a5572f5f8905a
Facundo <facugastonbarral@gmail.com>
2023-01-21 19:47:41 -0300

    Add Categories to Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header.js'
 import Banner from '../components/Banner.js'
+import Categories from '../components/Categories.js'
 
 
 const Home: NextPage = () => {
@@ -13,7 +14,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Header/>
-    <Banner/>
+    {/* <Banner/> */}
+    <Categories/>
+
+
     </div>
   )
 }
