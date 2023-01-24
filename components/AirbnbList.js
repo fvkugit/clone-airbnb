904cebb428a3c81a40918e63f8a1117d304c8c30
Facundo <facugastonbarral@gmail.com>
2023-01-23 21:13:05 -0300

    Created AirbnbList component

---
+++ components/AirbnbList.js
@@ -0,0 +1,286 @@
+import Image from "next/image";
+import { HeartIcon } from "@heroicons/react/24/solid";
+
+function AirbnbList() {
+  return (
+    <div className="mx-6 grid gap-5 grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 lg:mx-8 xl:grid-cols-5 2xl:grid-cols-6">
+
+      <div className="flex items-start flex-col aspect-square">
+        <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+            <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/novaprata/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">Nova Prata, Brasil</p>
+        <p className="text-gray-500">979 kilometers away</p>
+        <p className="text-gray-500">Feb 7 - 14</p>
+        <p>
+          <strong>$71 USD </strong>night
+        </p>
+      </div>
+
+      <div className="flex items-start flex-col aspect-square">
+      <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+          <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/sanpedro/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">San Pedro de Atacama, Chile</p>
+        <p className="text-gray-500">1,344 kilometers away</p>
+        <p className="text-gray-500">Feb 10 - 15</p>
+        <p>
+          <strong>$172 USD </strong>night
+        </p>
+      </div>
+      <div className="flex items-start flex-col aspect-square">
+        <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+            <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/novaprata/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">Nova Prata, Brasil</p>
+        <p className="text-gray-500">979 kilometers away</p>
+        <p className="text-gray-500">Feb 7 - 14</p>
+        <p>
+          <strong>$71 USD </strong>night
+        </p>
+      </div>
+
+      <div className="flex items-start flex-col aspect-square">
+      <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+          <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/sanpedro/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">San Pedro de Atacama, Chile</p>
+        <p className="text-gray-500">1,344 kilometers away</p>
+        <p className="text-gray-500">Feb 10 - 15</p>
+        <p>
+          <strong>$172 USD </strong>night
+        </p>
+      </div>
+      <div className="flex items-start flex-col aspect-square">
+        <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+            <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/novaprata/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">Nova Prata, Brasil</p>
+        <p className="text-gray-500">979 kilometers away</p>
+        <p className="text-gray-500">Feb 7 - 14</p>
+        <p>
+          <strong>$71 USD </strong>night
+        </p>
+      </div>
+
+      <div className="flex items-start flex-col aspect-square">
+      <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+          <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/sanpedro/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">San Pedro de Atacama, Chile</p>
+        <p className="text-gray-500">1,344 kilometers away</p>
+        <p className="text-gray-500">Feb 10 - 15</p>
+        <p>
+          <strong>$172 USD </strong>night
+        </p>
+      </div>
+      <div className="flex items-start flex-col aspect-square">
+        <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+            <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/novaprata/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">Nova Prata, Brasil</p>
+        <p className="text-gray-500">979 kilometers away</p>
+        <p className="text-gray-500">Feb 7 - 14</p>
+        <p>
+          <strong>$71 USD </strong>night
+        </p>
+      </div>
+
+      <div className="flex items-start flex-col aspect-square">
+      <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+          <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/sanpedro/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">San Pedro de Atacama, Chile</p>
+        <p className="text-gray-500">1,344 kilometers away</p>
+        <p className="text-gray-500">Feb 10 - 15</p>
+        <p>
+          <strong>$172 USD </strong>night
+        </p>
+      </div>
+      <div className="flex items-start flex-col aspect-square">
+        <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+            <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/novaprata/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">Nova Prata, Brasil</p>
+        <p className="text-gray-500">979 kilometers away</p>
+        <p className="text-gray-500">Feb 7 - 14</p>
+        <p>
+          <strong>$71 USD </strong>night
+        </p>
+      </div>
+
+      <div className="flex items-start flex-col aspect-square">
+      <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+          <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/sanpedro/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">San Pedro de Atacama, Chile</p>
+        <p className="text-gray-500">1,344 kilometers away</p>
+        <p className="text-gray-500">Feb 10 - 15</p>
+        <p>
+          <strong>$172 USD </strong>night
+        </p>
+      </div>
+      <div className="flex items-start flex-col aspect-square">
+        <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+            <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/novaprata/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">Nova Prata, Brasil</p>
+        <p className="text-gray-500">979 kilometers away</p>
+        <p className="text-gray-500">Feb 7 - 14</p>
+        <p>
+          <strong>$71 USD </strong>night
+        </p>
+      </div>
+
+      <div className="flex items-start flex-col aspect-square">
+      <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+          <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/sanpedro/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">San Pedro de Atacama, Chile</p>
+        <p className="text-gray-500">1,344 kilometers away</p>
+        <p className="text-gray-500">Feb 10 - 15</p>
+        <p>
+          <strong>$172 USD </strong>night
+        </p>
+      </div>
+      <div className="flex items-start flex-col aspect-square">
+        <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+            <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/novaprata/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">Nova Prata, Brasil</p>
+        <p className="text-gray-500">979 kilometers away</p>
+        <p className="text-gray-500">Feb 7 - 14</p>
+        <p>
+          <strong>$71 USD </strong>night
+        </p>
+      </div>
+
+      <div className="flex items-start flex-col aspect-square">
+      <div className="h-full w-full relative">
+          <div className="absolute right-3 top-3 h-7 w-7 z-10">
+          <HeartIcon className="opacity-70 stroke-white stroke-2 hover:scale-110 transition duration-150 cursor-pointer" />
+          </div>
+          <Image
+            src="/images/airbnbs/sanpedro/1.webp"
+            fill
+            className=" rounded-2xl"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        </div>
+        <p className="font-semibold">San Pedro de Atacama, Chile</p>
+        <p className="text-gray-500">1,344 kilometers away</p>
+        <p className="text-gray-500">Feb 10 - 15</p>
+        <p>
+          <strong>$172 USD </strong>night
+        </p>
+      </div>
+      
+
+    </div>
+  );
+}
+export default AirbnbList;
